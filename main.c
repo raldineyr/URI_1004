cf5331da2e709f657ae13b4312e1bd53aef0cc32
raldineyr <64384382+raldineyr@users.noreply.github.com>
2020-06-08 22:29:26 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A, B, PROD;
+
+
+    scanf("%d\n",&A);
+    scanf("%d",&B);
+
+    printf("PROD = %d\n",PROD=A*B);
+    return 0;
+}
